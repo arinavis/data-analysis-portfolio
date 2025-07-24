e009876be899388a1075451bdee61af6386c2678
arinavis <avisnevskaja@gmail.com>
2025-07-24 12:05:41 +0300

    Updated knowledge on variables and data types

---
+++ python/Restoring basics/python_RestoringBasics.py
@@ -0,0 +1,15 @@
+# Task nr.1. Variables and data types
+#Objective: Learn to store data in variables and display it on the screen.
+
+# Create variables
+name = 'Arina'  # Text (str)
+age = 29       # Numeric (int)
+height = 1.69  # Numeric (float)
+is_junior = True  # Boolean (Bool)
+location = 'Vilnius, Lithuania' # Text (str)
+
+# Displaying variables on display
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is a junior data analytic", is_junior)
